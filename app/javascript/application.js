cf2cd6c82bf183faccd7233c976c2d41fe5e79bf
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-06-27 13:14:22 -0400

    Add highlighting to trix editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+import Trix from "trix"
+
+Trix.config.textAttributes.highlight = { tagName: "mark" };
+
+addEventListener("trix-initialize", function(event) {
+	var groupElement = event.target.toolbarElement.querySelector(".trix-button-group.trix-button-group--text-tools")
+
+	groupElement.insertAdjacentHTML("beforeend", '<button type="button" class="trix-button trix-button--icon trix-button--icon-highlight" data-trix-attribute="highlight" data-trix-key="y" title="Highlight" tabindex="-1">${lang.highlight}')
+})
